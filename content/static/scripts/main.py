82ed5abd544c4c9fc44a5890780fafbaad432e5d
GiovanniMP <giovanni_montevechi94@hotmail.com>
2022-09-26 13:41:08 -0300

    update billing and stock send emails

--- content/static/scripts/main.py
+++ content/static/scripts/main.py
@@ -211,14 +211,11 @@ def createBilling():
 
         pix = generateQRCode(all[0], all[3], all[4], str(total), all[1])
 
-        qr = f"C:\ProjetosGit\lab-projetos\content\static\generatedpix/pix-{all[1]}.png"
+        qr = f"./content/static/generatedpix/pix-{all[1]}.png"
 
         sendBillingMail(qr, currentClient, currentUser, currentProduct, total)
-        print(7)
 
     print("Fechando Faturamento")
-
-# FALTA ARRUMAR O VALOR PASSADO PARA O PIX QRCODE
 
 
 def generateQRCode(userPix, city, username, value, clientName):
@@ -226,7 +223,7 @@ def generateQRCode(userPix, city, username, value, clientName):
 
     if pix.is_valid():
         pix.save_qrcode(
-            "C:\ProjetosGit\lab-projetos\content\static\generatedpix", f"pix-{clientName}")
+            "./content/static/generatedpix", f"pix-{clientName}")
         return pix.generate_code()
     else:
         return "Dados para Pix inválidos."
@@ -235,13 +232,14 @@ def generateQRCode(userPix, city, username, value, clientName):
 # --------------- Envio de Email ---------------
 
 def sendStockAlertMail(product, user):
-    user = 'labprojetospuc@gmail.com'
+    app_email = 'labprojetospuc@gmail.com'
     app_password = 'npfyvwfaljvlvplv'  # Token for gmail
     to = user.email
 
     productList = product.name
     amountList = product.amount
     priceList = product.unitPrice
+
 
     list = ""
 
@@ -258,7 +256,7 @@ def sendStockAlertMail(product, user):
                lista,
                f'</article></section></div><footer style="margin-top: 10px;color: rgb(143, 143, 143);font-family: sans-serif;"><p style="padding: 0px;margin: 5px;">Ward - Automated Billing Service</p><p style="padding: 0px;margin: 5px;">Av. Padre Cletus Francis Cox, 1661 - Country Club, Poços de Caldas - MG, 37714-620</p><p style="padding: 0px;margin: 5px;">Nos contate em labprojetospuc@gmail.com</p></footer></div>']
 
-    with yagmail.SMTP(user, app_password) as yag:
+    with yagmail.SMTP(app_email, app_password) as yag:
         yag.send(to, subject, content)
         print('Sent email successfully')
 
@@ -271,21 +269,27 @@ def sendBillingMail(qr, client, user, product, total):
     productList = product.name
     amountList = product.amount
     priceList = product.unitPrice
+    total = str(total)[:-2]
+    virgula = ',00'
+    total = total + virgula
 
     list = ""
+    lista = []
 
     for num in range(len(productList)):
-        lista = list + \
-            f'<span style="border-bottom: 1px solid lightgray;display: inline-block;width: 100%;"><p style="text-align: left;display: inline-block;width: 70%;">{productList[num]}</p><i>{amountList[num]}</i><a style="text-align: right;display: inline-block;width: 15%;position: relative;">R${priceList[num]}</a></span>'
+        list = f'<span style="border-bottom: 1px solid lightgray;display: inline-block;width: 100%;"><p style="text-align: left;display: inline-block;width: 70%;">{productList[num]}</p><i>{amountList[num]}</i><a style="text-align: right;display: inline-block;width: 15%;position: relative;">R${priceList[num]}</a></span>'
+        lista.append(list)
 
+    items = ["&emsp;{}".format(s) for s in lista]
+    items = "".join(items)
     subject = f'Cobrança à {user.name}'
     content = ['<div style="text-align: center;background-color: #e1e1e1;"><div>',
                f'<h2>Olá {client.name},</h2><p style="font-size: large">Este é um email referente à cobrança gerada por {user.name}. Em anexo segue o código pix referente ao valor de R${total}.</p></div>',
                f'<div style="border: solid 1px gray; width: 1000px; margin: 0 auto; background-color: white; box-shadow: 2px 2px #86868621"><header>',
                yagmail.inline("./content/static/img/title.png"),
-               f'</header><section><article><h1>Resumo da compra</h1><div style="width: 100%;"><a style="width: 65%; padding-left: 3%;display: inline-block;position: relative;font-weight: bold;text-align: left;">Produto</a><a style="width: 12.5%; padding-left: 2.5%;display: inline-block;position: relative;font-weight: bold;text-align: left;">Quantidade</a><a style="width: 9%; padding-left: 1%;display: inline-block;position: relative;font-weight: bold;text-align: left;">Preço(un.)</a></div>',
-               lista,
-               f'<div style="border-top: 2px solid black"><p style="width: 95%;text-align: right;font-family: sans-serif;font-size: large;font-weight: bold;">Total: R$30,00</p></div></article></section></div><footer style="margin-top: 10px;color: rgb(143, 143, 143);font-family: sans-serif;"><p style="padding: 0px;margin: 5px;">Ward - Automated Billing Service</p><p style="padding: 0px;margin: 5px;">Av. Padre Cletus Francis Cox, 1661 - Country Club, Poços de Caldas - MG, 37714-620</p><p style="padding: 0px;margin: 5px;">Nos contate em labprojetospuc@gmail.com</p></footer></div>']
+               f'</header><section><article><h1>Resumo da compra</h1><div style="width: 100%;"><a style="width: 65%; padding-left: 3%;display: inline-block;position: relative;font-weight: bold;text-align: left;">Produto</a><a style="width: 12.5%; padding-left: 2.5%;display: inline-block;position: relative;font-weight: bold;text-align: left;">Quantidade</a><a style="width: 9%; padding-left: 1%;display: inline-block;position: relative;font-weight: bold;text-align: left;">Preço</a></div>',
+               items,
+               f'<div style="border-top: 2px solid black"><p style="width: 95%;text-align: right;font-family: sans-serif;font-size: large;font-weight: bold;">Total: R${total}</p></div></article></section></div><footer style="margin-top: 10px;color: rgb(143, 143, 143);font-family: sans-serif;"><p style="padding: 0px;margin: 5px;">Ward - Automated Billing Service</p><p style="padding: 0px;margin: 5px;">Av. Padre Cletus Francis Cox, 1661 - Country Club, Poços de Caldas - MG, 37714-620</p><p style="padding: 0px;margin: 5px;">Nos contate em labprojetospuc@gmail.com</p></footer></div>']
     attachments = [qr]
 
     with yagmail.SMTP(userEmail, app_password) as yag:
@@ -299,20 +303,20 @@ def checkStock():
     connection = create_connection(
         "us-cdbr-east-06.cleardb.net", "b090112be85288", "2f84fdce", "heroku_7324e25c80c3d90")
     cursor = connection.cursor()
-    stmt = ("SELECT GROUP_CONCAT(price),GROUP_CONCAT(product),GROUP_CONCAT(amount),userEmail FROM stock,data WHERE amount = 0 group by product")
+    stmt = ("SELECT GROUP_CONCAT(price),GROUP_CONCAT(product),GROUP_CONCAT(amount),userEmail,userName FROM stock,data WHERE amount = 0 group by product")
     cursor.execute(stmt)
     row = cursor.fetchall()
     for all in row:
-        currentUser = User("null", all[3], "pix")
+        currentUser = User(all[4], all[3], "pix")
         currentProduct = Product(all[1].split(
             ','), all[2].split(','), all[0].split(','))
         sendStockAlertMail(currentProduct, currentUser)
 
-    stmt = ("SELECT GROUP_CONCAT(price),GROUP_CONCAT(product),GROUP_CONCAT(amount),userEmail FROM stock,data WHERE amount = warning GROUP BY product,amount,userEmail")
+    stmt = ("SELECT GROUP_CONCAT(price),GROUP_CONCAT(product),GROUP_CONCAT(amount),userEmail,userName FROM stock,data WHERE amount = warning GROUP BY product,amount,userEmail")
     cursor.execute(stmt)
     row = cursor.fetchall()
     for all in row:
-        currentUser = User("null", all[3], "pix")
+        currentUser = User(all[4], all[3], "pix")
         currentProduct = Product(all[1].split(
             ','), all[2].split(','), all[0].split(','))
         sendStockAlertMail(currentProduct, currentUser)
@@ -326,10 +330,10 @@ def callDailyRoutines():
         createBilling()
     except ValueError:
         print("Erro ao gerar faturamento!")
-    # try:
-    #     checkStock()
-    # except ValueError:
-    #     print("Erro ao realizar a verificação do estoque!")
+    try:
+        checkStock()
+    except ValueError:
+        print("Erro ao realizar a verificação do estoque!")
 
 
 callDailyRoutines()
