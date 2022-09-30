fb66732f5b22e8a58bb7e764fed5f1b6ebd7b293
GiovanniMP <giovanni_montevechi94@hotmail.com>
2022-09-29 21:29:18 -0300

    update sendstock

--- main.py
+++ main.py
@@ -266,16 +266,7 @@ def checkStock():
     connection = create_connection(
         "us-cdbr-east-06.cleardb.net", "b090112be85288", "2f84fdce", "heroku_7324e25c80c3d90")
     cursor = connection.cursor()
-    stmt = ("SELECT GROUP_CONCAT(price),GROUP_CONCAT(product),GROUP_CONCAT(amount),userEmail,userName FROM stock,data WHERE amount = 0 group by product")
-    cursor.execute(stmt)
-    row = cursor.fetchall()
-    for all in row:
-        currentUser = User(all[4], all[3], "pix")
-        currentProduct = Product(all[1].split(
-            ','), all[2].split(','), all[0].split(','))
-        sendStockAlertMail(currentProduct, currentUser)
-
-    stmt = ("SELECT GROUP_CONCAT(price),GROUP_CONCAT(product),GROUP_CONCAT(amount),userEmail,userName FROM stock,data WHERE amount = warning GROUP BY product,amount,userEmail")
+    stmt = ("SELECT GROUP_CONCAT(price),GROUP_CONCAT(product),GROUP_CONCAT(amount),userEmail,userName FROM stock,data WHERE amount <= warning GROUP BY product,amount,userEmail")
     cursor.execute(stmt)
     row = cursor.fetchall()
     for all in row:
