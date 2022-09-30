7625f117e6c4ae2c4d0e8144313996408151fff8
Rodrigo <rscamargo@gmail.com>
2022-09-29 21:19:38 -0300

    Update Interface

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 # pip install pixqrcode
 # pip install pillow
 # pip install openpyxl
+# pip install flask-bootstrap
 
 from xml.etree.ElementTree import tostring
 from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
 from pixqrcode import PixQrCode
 from openpyxl import load_workbook
 from mysql.connector import Error
@@ -102,7 +104,10 @@ def readExpenseFile(file):
 # --------------- Arquivo de Estoque ---------------
 def readStockFile(file):
     for value in file.iter_rows(min_row=4, min_col=3, max_col=6, values_only=True):
-        updateStockAndSave(value[0], value[1], id, value[3], value[2])
+        if (value[0] != None):
+            updateStockAndSave(value[0], value[1], id, value[3], value[2])
+        else:
+            break
 
 # endregion
 
@@ -185,7 +190,22 @@ def updateStockAndSave(insumo, quantidade, id, alerta, preco=0):
 
 
 def cleanDatabase():
-    return
+    connection = create_connection(
+        "us-cdbr-east-06.cleardb.net", "b090112be85288", "2f84fdce", "heroku_7324e25c80c3d90")
+    cursor = connection.cursor()
+    stmt = ("DELETE FROM cons WHERE clientName='Cliente Apresentacao'")
+    cursor.execute(stmt)
+    connection.commit()
+    stmt = ("DELETE FROM data WHERE clientName='Cliente Apresentacao';")
+    cursor.execute(stmt)
+    connection.commit()
+    stmt = ("DELETE FROM stock WHERE product = 'Produto 1';")
+    cursor.execute(stmt)
+    connection.commit()
+    stmt = ("DELETE FROM stock WHERE product = 'Produto 2';")
+    cursor.execute(stmt)
+    connection.commit()
+    print("Remoções realizadas no banco")
 
 # endregion
 
@@ -223,7 +243,7 @@ def createBilling():
 
         pix = generateQRCode(all[0], all[3], all[4], str(total), all[1])
 
-        qr = f"/content/static/generatedpix/pix-{all[1]}.png"
+        qr = f"./static/generatedpix/pix-{all[1]}.png"
 
         sendBillingMail(qr, currentClient, currentUser, currentProduct, total)
 
@@ -235,7 +255,7 @@ def generateQRCode(userPix, city, username, value, clientName):
 
     if pix.is_valid():
         pix.save_qrcode(
-            "/content/static/generatedpix", f"pix-{clientName}")
+            "./static/generatedpix", f"pix-{clientName}")
         return pix.generate_code()
     else:
         return "Dados para Pix inválidos."
@@ -289,7 +309,7 @@ def sendStockAlertMail(product, user):
     content = ['<div style="text-align: center;background-color: #e1e1e1;"><div>',
                f'<h2>Olá {user.name},</h2><p style="font-size: large">Este é um email de aviso referente aos produtos que estão acabando de seu estoque. <br> Para adicionar mais no sistema, utilize a mesma template de estoque APENAS com os produtos que irá acrescentar a ele.</p></div>',
                f'<div style="border: solid 1px gray; width: 1000px; margin: 0 auto; background-color: white; box-shadow: 2px 2px #86868621"><header>',
-               yagmail.inline("/content/static/img/title.png"),
+               yagmail.inline("./static/img/title.png"),
                f'</header><section><article><h1>Estoque abaixo do limite</h1><div style="width: 100%;"><a style="width: 65%; padding-left: 3%;display: inline-block;position: relative;font-weight: bold;text-align: left;">Produto</a><a style="width: 12.5%; padding-left: 2.5%;display: inline-block;position: relative;font-weight: bold;text-align: left;">Quantidade</a><a style="width: 9%; padding-left: 1%;display: inline-block;position: relative;font-weight: bold;text-align: left;">Preço(un.)</a></div>',
                lista,
                f'</article></section></div><footer style="margin-top: 10px;color: rgb(143, 143, 143);font-family: sans-serif;"><p style="padding: 0px;margin: 5px;">Ward - Automated Billing Service</p><p style="padding: 0px;margin: 5px;">Av. Padre Cletus Francis Cox, 1661 - Country Club, Poços de Caldas - MG, 37714-620</p><p style="padding: 0px;margin: 5px;">Nos contate em labprojetospuc@gmail.com</p></footer></div>']
@@ -324,12 +344,12 @@ def sendBillingMail(qr, client, user, product, total):
     content = ['<div style="text-align: center;background-color: #e1e1e1;"><div>',
                f'<h2>Olá {client.name},</h2><p style="font-size: large">Este é um email referente à cobrança gerada por {user.name}. Em anexo segue o código pix referente ao valor de R${total}.</p></div>',
                f'<div style="border: solid 1px gray; width: 1000px; margin: 0 auto; background-color: white; box-shadow: 2px 2px #86868621"><header>',
-               yagmail.inline("/content/static/img/title.png"),
+               yagmail.inline("./static/img/title.png"),
                f'</header><section><article><h1>Resumo da compra</h1><div style="width: 100%;"><a style="width: 65%; padding-left: 3%;display: inline-block;position: relative;font-weight: bold;text-align: left;">Produto</a><a style="width: 12.5%; padding-left: 2.5%;display: inline-block;position: relative;font-weight: bold;text-align: left;">Quantidade</a><a style="width: 9%; padding-left: 1%;display: inline-block;position: relative;font-weight: bold;text-align: left;">Preço</a></div>',
                items,
                f'<div style="border-top: 2px solid black"><p style="width: 95%;text-align: right;font-family: sans-serif;font-size: large;font-weight: bold;">Total: R${total}</p></div></article></section></div>',
                yagmail.inline(
-                   f"/content/static/generatedpix/pix-{client.name}.png"),
+                   f"./static/generatedpix/pix-{client.name}.png"),
                '<footer style="margin-top: 10px;color: rgb(143, 143, 143);font-family: sans-serif;"><p style="padding: 0px;margin: 5px;">Ward - Automated Billing Service</p><p style="padding: 0px;margin: 5px;">Av. Padre Cletus Francis Cox, 1661 - Country Club, Poços de Caldas - MG, 37714-620</p><p style="padding: 0px;margin: 5px;">Nos contate em labprojetospuc@gmail.com</p></footer></div>']
     attachments = [qr]
 
@@ -349,7 +369,7 @@ app.config['UPLOAD_FOLDER'] = "static/uploadedexcel/"
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', username=username)
 
 
 @app.route('/upload', methods=['POST', 'GET'])
@@ -384,16 +404,15 @@ def daily():
     if request.method == 'POST':
         if request.form['help'] == 'Call Routines':
             try:
-                # createBilling()
-                return "Create Billing"
+                createBilling()
             except ValueError:
-                print("Erro ao gerar faturamento!")
+                return "Erro ao gerar faturamento!"
             try:
-                # checkStock()
-                return "Check Stock"
+                checkStock()
             except ValueError:
-                print("Erro ao realizar a verificação do estoque!")
+                return "Erro ao realizar a verificação do estoque!"
 
+            return "Faturamento e checagem de estoque rodados com sucesso!!"
         elif request.form['help'] == 'Clean DB':
             cleanDatabase()
             return "Dados de teste excluidos do DB"
